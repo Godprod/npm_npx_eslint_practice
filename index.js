b7fa5bdf8a3ab5ce8d3d87a9271d83fe6718cc5c
Dmitriy <digavrprod@gmail.com>
2023-04-06 05:35:16 +0300

    done hw

---
+++ index.js
@@ -0,0 +1,16 @@
+'use strict';
+function sum(a, b) {
+  const result = a + b;
+  return result;
+}
+sum(1, 1);
+function sub(a, b) {
+  const result = a - b;
+  return result;
+}
+sub(1, 1);
+function multi(a, b) {
+  const result = a * b;
+  return result;
+}
+multi(1, 1);
